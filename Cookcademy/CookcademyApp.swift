554e78d3b1b815bcc5123db2a826bfd2aaf26ad8
Noevenzahn <info.artuofficial@gmail.com>
2021-08-31 20:42:44 +0200

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Melvin Waberski on 31.08.21.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
